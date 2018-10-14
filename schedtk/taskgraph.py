e8c708af79dad6096f60c59314f84e1646eb090a
Stanislav Bohm <stanislav.bohm@vsb.cz>
2018-10-14 22:00:53 +0200

    Dot export

--- schedtk/taskgraph.py
+++ schedtk/taskgraph.py
@@ -46,5 +46,21 @@ class TaskGraph:
             for t in task.consumers:
                 assert t in self.tasks
 
+    def to_dot(self, name, verbose=False):
+        stream = ["digraph ", name, " {\n"]
+
+        for task in self.tasks:
+            label = "{}\\n{:.2f}/{:.2f}".format(task.label, task.duration, task.size)
+            stream.append("v{} [label=\"{}\"]\n".format(task.id, label))
+            for t in task.inputs:
+                stream.append("v{} -> v{}\n".format(t.id, task.id))
+        stream.append("}\n")
+        return "".join(stream)
+
+    def write_dot(self, filename):
+        dot = self.to_dot("g")
+        with open(filename, "w") as f:
+            f.write(dot)
+
     def __repr__(self):
         return "<TaskGraph #t={}>".format(len(self.tasks))
