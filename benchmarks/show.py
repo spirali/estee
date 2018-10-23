cbdba6e3e50993e715dd3be126ee68a0e24672e0
Stanislav Bohm <stanislav.bohm@vsb.cz>
2018-10-23 15:49:46 +0200

    violin in show

--- benchmarks/show.py
+++ benchmarks/show.py
@@ -3,14 +3,26 @@ import matplotlib.pyplot as plt
 import seaborn
 import argparse
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("dataset")
+    parser.add_argument("--heatmap", action="store_true")
+    parser.add_argument("--violin", action="store_true")
     return parser.parse_args()
+
 
 def draw_heatmap(*args, **kw):
     df = kw["data"].groupby(["bandwidth"])[kw["avg_columns"]].mean()
     seaborn.heatmap(df, cmap='RdYlGn_r', linewidths=0.5, annot=True)
+
+
+def draw_violin(*args, **kw):
+    df = kw["data"][kw["avg_columns"]]
+    df = df.melt(var_name='groups', value_name='vals')
+    g = seaborn.violinplot(data=df, x="groups", y="vals")
+    g.set_yscale('log')
+
 
 def main():
     args = parse_args()
@@ -27,9 +39,13 @@ def main():
     df = data.groupby(["cluster_name", "bandwidth"])[avg_columns].mean()
     print(df)
 
-    fg = seaborn.FacetGrid(data, col='cluster_name')
-    fg.map_dataframe(draw_heatmap, avg_columns=avg_columns) #, "bandwidth", *avg_columns)
-#seaborn.heatmap(df, cmap='RdYlGn_r', linewidths=0.5, annot=True)
+    if args.heatmap:
+        fg = seaborn.FacetGrid(data, col='cluster_name')
+        fg.map_dataframe(draw_heatmap, avg_columns=avg_columns)
+
+    if args.violin:
+        fg = seaborn.FacetGrid(data, col='cluster_name', row='bandwidth')
+        fg.map_dataframe(draw_violin, avg_columns=avg_columns)
 
     plt.show()
 
