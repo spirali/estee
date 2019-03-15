4941fe8bbde54bf737aaac9cb3b7c0621bc715a1
Stanislav Bohm <stanislav.bohm@vsb.cz>
2019-03-15 16:10:30 +0100

    commands.py removed

--- estee/simulator/__init__.py
+++ estee/simulator/__init__.py
@@ -1,5 +1,4 @@
 
-from .simulator import Simulator  # noqa
-from .commands import TaskAssignment  # noqa
+from .simulator import Simulator, TaskAssignment  # noqa
 from .netmodels import InstantNetModel, SimpleNetModel, MaxMinFlowNetModel  # noqa
 from .worker import Worker  # noqa
--- estee/simulator/simulator.py
+++ estee/simulator/simulator.py
@@ -1,7 +1,6 @@
 
 from simpy import Environment, Event
 
-from .commands import TaskAssignment
 from .runtimeinfo import RuntimeState, TaskState
 from .trace import TaskAssignTraceEvent
 
@@ -10,6 +9,21 @@ import logging
 logger = logging.getLogger(__name__)
 
 from ..simulator.trace import FetchEndTraceEvent
+
+
+class TaskAssignment:
+
+    __slots__ = ("worker", "task", "priority", "block", "cancelled", "remaining_inputs_count")
+
+    def __init__(self, worker, task, priority=0, block=0):
+        assert block <= priority
+        self.worker = worker
+        self.task = task
+        self.priority = priority
+        self.block = block
+        self.cancelled = False
+        self.remaining_inputs_count = None
+
 
 class Simulator:
 
